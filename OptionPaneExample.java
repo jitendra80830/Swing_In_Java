267e29e61066a9f38a455247b09ea6667cf27087
jitendra <jitendrakumar89973@gmail.com>
2021-02-06 15:37:41 +0530

    Example of JOptionPane

---
+++ OptionPaneExample.java
@@ -0,0 +1,14 @@
+//Example of JDiolog
+import javax.swing.*;
+
+public class OptionPaneExample{
+	JFrame f;
+	OptionPaneExample(){
+		f=new JFrame();
+		JOptionPane.showMessageDialog(f,"Hello,welcome to IIt Khadp");
+	}
+	public static void main(String[] args){
+		new OptionPaneExample();
+	}
+}
+	
