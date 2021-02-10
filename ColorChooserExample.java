f7aea72345b69d4d8d930ee76aa1653d3195b8bc
jitendra <jitendrakumar89973@gmail.com>
2021-02-10 18:00:28 +0530

    ColorChooser Example

---
+++ ColorChooserExample.java
@@ -0,0 +1,27 @@
+//Example of Colorchooser
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ColorChooserExample extends JFrame implements ActionListener{
+	JButton b;
+	Container c;
+	ColorChooserExample(){
+		c=getContentPane();
+		c.setLayout(new FlowLayout());
+		b=new JButton("color");
+		b.addActionListener(this);
+		c.add(b);
+	}
+	public void actionPerformed(ActionEvent e){
+		Color initialcolor=Color.RED;
+		Color color=JColorChooser.showDialog(this,"selsect the color",initialcolor);
+		c.setBackground(color);
+	}
+	public static void main(String[] args){
+		ColorChooserExample ch=new ColorChooserExample();
+		ch.setSize(400,500);
+		ch.setVisible(true);
+		ch.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+}
